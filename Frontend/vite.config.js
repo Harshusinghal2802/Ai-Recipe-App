625d40c5edc5826870050cde389e547d1bcb0119
Harshusinghal2802 <harshsinghal2820@gmail.com>
2026-05-26 14:35:51 +0530

    update project

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -17,12 +17,12 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/pwa-192.png',
+            src: '/image.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/pwa-512.png',
+            src: '/image.png',
             sizes: '512x512',
             type: 'image/png'
           }
